bd53b918861276081c6b4107b5d0e7b1b8cfcb6e
lolster <sushrith.arkal@gmail.com>
2016-07-17 00:09:50 +0530

    Delete Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-import java.lang.*;
-
-public class Test {
-	public static void main(String[] args) {
-		double val = 20.0 - (949.0/1024.0);
-		System.out.println(val + " " + Math.round(val*100.0)/100.0);
-	}
-}
